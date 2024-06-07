03f71ac3b89c86bf6637f585e31cd071ddc0f86b
VeterAN <vet-an@yandex.ru>
2024-06-07 09:03:33 +0000

    On branch 2274af5 Changes to be committed: 	modified:   src/main.py
    Changes not staged for commit:
    	modified:   .env
    	modified:   .vscode/settings.json
    	modified:   src/__pycache__/config.cpython-310.pyc
    	modified:   src/__pycache__/database.cpython-310.pyc
    	modified:   src/__pycache__/main.cpython-310.pyc
    	modified:   src/config.py
    	modified:   src/dao/__pycache__/base.cpython-310.pyc
    	modified:   src/dao/base.py
    	modified:   src/database.py
    	modified:   src/main.py
    	modified:   src/migrations/__pycache__/env.cpython-310.pyc
    	modified:   src/migrations/env.py
    	modified:   src/migrations/versions/__pycache__/737285099091_initial_db.cpython-310.pyc
    	modified:   src/tasks/__pycache__/dao.cpython-310.pyc
    	modified:   src/tasks/__pycache__/model.cpython-310.pyc
    	modified:   src/tasks/__pycache__/router.cpython-310.pyc
    	modified:   src/tasks/__pycache__/schemas.cpython-310.pyc
    	modified:   src/tasks/copy_extensions/__pycache__/dao.cpython-310.pyc
    	modified:   src/tasks/copy_extensions/__pycache__/model.cpython-310.pyc
    	modified:   src/tasks/copy_extensions/dao.py
    	modified:   src/tasks/copy_extensions/model.py
    	modified:   src/tasks/dao.py
    	modified:   src/tasks/from_dirs/__pycache__/dao.cpython-310.pyc
    	modified:   src/tasks/from_dirs/__pycache__/model.cpython-310.pyc
    	modified:   src/tasks/from_dirs/__pycache__/schemas.cpython-310.pyc
    	modified:   src/tasks/from_dirs/dao.py
    	modified:   src/tasks/from_dirs/model.py
    	modified:   src/tasks/from_dirs/schemas.py
    	modified:   src/tasks/last_operations/__pycache__/dao.cpython-310.pyc
    	modified:   src/tasks/last_operations/__pycache__/model.cpython-310.pyc
    	modified:   src/tasks/last_operations/__pycache__/schemas.cpython-310.pyc
    	modified:   src/tasks/last_operations/dao.py
    	modified:   src/tasks/last_operations/model.py
    	modified:   src/tasks/last_operations/schemas.py
    	modified:   src/tasks/model.py
    	modified:   src/tasks/router.py
    	modified:   src/tasks/schemas.py
    	modified:   src/tasks/to_dirs/__pycache__/dao.cpython-310.pyc
    	modified:   src/tasks/to_dirs/__pycache__/model.cpython-310.pyc
    	modified:   src/tasks/to_dirs/__pycache__/schemas.cpython-310.pyc
    	modified:   src/tasks/to_dirs/dao.py
    	modified:   src/tasks/to_dirs/model.py
    	modified:   src/tasks/to_dirs/schemas.py
    
    Untracked files:
    	src/__pycache__/exceptions.cpython-310.pyc
    	src/exceptions.py
    	src/migrations/versions/1c92308b8434_edit_model_schemas.py
    	src/migrations/versions/524dcda10cb5_edit_model_tasks.py
    	src/migrations/versions/54d7ab57745f_initial.py
    	src/migrations/versions/7049ab408a04_init.py
    	src/migrations/versions/__pycache__/1a5ba52d90c5_add_table.cpython-310.pyc
    	src/migrations/versions/__pycache__/1c92308b8434_edit_model_schemas.cpython-310.pyc
    	src/migrations/versions/__pycache__/307bb6f12261_change_table_from_dir.cpython-310.pyc
    	src/migrations/versions/__pycache__/524dcda10cb5_edit_model_tasks.cpython-310.pyc
    	src/migrations/versions/__pycache__/54d7ab57745f_initial.cpython-310.pyc
    	src/migrations/versions/__pycache__/56fccfd50653_change_table_task.cpython-310.pyc
    	src/migrations/versions/__pycache__/68a93156a067_change_tables.cpython-310.pyc
    	src/migrations/versions/__pycache__/7049ab408a04_init.cpython-310.pyc
    	src/migrations/versions/__pycache__/78bcbe2839d9_drop_table_from_extension_not_copy.cpython-310.pyc
    	src/migrations/versions/__pycache__/a90eeb4405ae_initial_migrations.cpython-310.pyc
    	src/migrations/versions/__pycache__/ac0d4e009347_edit_model_schemas.cpython-310.pyc
    	src/migrations/versions/__pycache__/ebcb6ad9ffc9_change_table_from_dir.cpython-310.pyc
    	src/migrations/versions/ac0d4e009347_edit_model_schemas.py

--- src/main.py
+++ src/main.py
@@ -18,5 +18,6 @@ app.include_router(to_dirs_router)
 app.include_router(last_operations_router)
 app.include_router(copy_operations_router)
 
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
